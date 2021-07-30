2fefc8be29a6d60e2bdd9186adb7c8a87b9a4fd1
kitthread <13022437+kitthread@users.noreply.github.com>
2021-07-30 14:10:23 +0200

    init

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class PowerShellJavaDemo {
+
+	public static void main(String[] args) throws IOException {
+
+		String line;
+
+		String command = "powershell.exe & \"..\\ps1\\KIM-Check\"";
+
+		Process powerShellProcess = Runtime.getRuntime().exec(command);
+
+		powerShellProcess.getOutputStream().close();
+
+		BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
+		while ((line = stdout.readLine()) != null) {
+			System.out.println("Output: " + line);
+
+		}
+
+		stdout.close();
+
+		BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
+		while ((line = stderr.readLine()) != null) {
+			System.out.println("Output: " + line);
+		}
+		stderr.close();
+
+	}
+
+}
